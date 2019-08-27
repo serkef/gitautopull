9ebc23a0bc2814fb80958a7ebe363d104663ff9e
serkef <sergios.kefalas@blacklane.com>
2019-08-27 10:22:39 +0200

    Add cloning feature

--- gitautopull.sh
+++ gitautopull.sh
@@ -3,9 +3,17 @@
 git_dir="$HOME/gh"
 filename=".autopull"
 while read -r line; do
-    cd "$git_dir/$line"
-    echo "Starting git pull for repo: $line"
-    echo "Current branch: $(git branch | grep \* | cut -d ' ' -f2)"
-    git pull --rebase
-    echo ""
+    if [ -d $git_dir/$line ]; then
+	cd "$git_dir/$line"
+	echo "Starting git pull for repo: $line"
+	echo "Current branch: $(git branch | grep \* | cut -d ' ' -f2)"
+	git pull --rebase
+	echo ""
+    else
+	cd $git_dir
+	repo="git@github.com:blacklane/$line.git"
+	echo "Starting git clone for repo: $line assuming $repo"
+	git clone $repo
+	echo ""
+    fi
 done < "$git_dir/$filename"
