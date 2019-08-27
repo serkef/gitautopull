aadc0c5aeb6857adcb53123e27cdbdee560d66a1
serkef <sergios.kefalas@blacklane.com>
2019-08-27 11:16:57 +0200

    Add sort on file

--- gitautopull.sh
+++ gitautopull.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+# Args
 git_dir="$HOME/gh"
 filename=".autopull"
+
+# Sort 
+listfile="$git_dir/$filename"
+sort -o $listfile $listfile
+
 while read -r line; do
-    if [ -d $git_dir/$line ]; then
-	cd "$git_dir/$line"
-	echo "Starting git pull for repo: $line"
-	echo "Current branch: $(git branch | grep \* | cut -d ' ' -f2)"
-	git pull --rebase
-	echo ""
+
+    local_repo="${git_dir}/${line}"
+    if [ -d "${local_repo}" ]; then
+        # Pull --rebase
+        cd "${local_repo}"
+        echo "Starting git pull for repo: ${line}"
+        echo "Current branch: $(git branch | grep \* | cut -d ' ' -f2)"
+        git pull --rebase
+        echo ""
     else
-	cd $git_dir
-	repo="git@github.com:blacklane/$line.git"
-	echo "Starting git clone for repo: $line assuming $repo"
-	git clone $repo
-	echo ""
+        # Clone
+        cd "${git_dir}"
+        repo="git@github.com:blacklane/$line.git"
+        echo "Starting git clone for repo: $line assuming $repo"
+        git clone $repo
+        echo ""
     fi
-done < "$git_dir/$filename"
+done < "$listfile"
